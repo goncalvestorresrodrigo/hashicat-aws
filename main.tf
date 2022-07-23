c4e2c2e17b3cf15595d38d5ee91ffc34580473aa
Rodrigo Torres <goncalvestorresrodrigo@gmail.com>
2022-07-23 17:59:43 +0000

    Alteracao todos CIDR para ip Rod menos ssh

--- main.tf
+++ main.tf
@@ -46,14 +46,14 @@ resource "aws_security_group" "hashicat" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["189.63.7.79/32"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["189.63.7.79/32"]
   }
 
   egress {
